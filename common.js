8a8145681c38cadf4197368a96b48a99d0e44e04
Bogdan Stanciu <bogdan.stanciu91@gmail.com>
2015-09-21 17:05:19 +0100

    Stripping down to what I need

--- common.js
+++ common.js
@@ -131,7 +131,6 @@ var Game = {  // a modified version of the game loop from my previous boulderdas
         requestAnimationFrame(frame, canvas);
       }
       frame(); // lets get this party started
-      Game.playMusic();
     });
   },
 
@@ -175,7 +174,7 @@ var Game = {  // a modified version of the game loop from my previous boulderdas
 
   //---------------------------------------------------------------------------
 
-  stats: function(parentId, id) { // construct mr.doobs FPS counter - along with friendly good/bad/ok message box
+  stats: function(parentId, id) { // construct mr.doobs FPS counter
 
     var result = new Stats();
     result.domElement.id = id || 'stats';
@@ -183,8 +182,6 @@ var Game = {  // a modified version of the game loop from my previous boulderdas
 
     var msg = document.createElement('div');
     msg.style.cssText = "border: 2px solid gray; padding: 5px; margin-top: 5px; text-align: left; font-size: 1.15em; text-align: right;";
-    msg.innerHTML = "Your canvas performance is ";
-    Dom.get(parentId).appendChild(msg);
 
     var value = document.createElement('span');
     value.innerHTML = "...";
@@ -192,28 +189,8 @@ var Game = {  // a modified version of the game loop from my previous boulderdas
 
     setInterval(function() {
       var fps   = result.current();
-      var ok    = (fps > 50) ? 'good'  : (fps < 30) ? 'bad' : 'ok';
-      var color = (fps > 50) ? 'green' : (fps < 30) ? 'red' : 'gray';
-      value.innerHTML       = ok;
-      value.style.color     = color;
-      msg.style.borderColor = color;
     }, 5000);
     return result;
-  },
-
-  //---------------------------------------------------------------------------
-
-  playMusic: function() {
-    var music = Dom.get('music');
-    music.loop = true;
-    music.volume = 0.05; // shhhh! annoying music!
-    music.muted = (Dom.storage.muted === "true");
-    music.play();
-    Dom.toggleClassName('mute', 'on', music.muted);
-    Dom.on('mute', 'click', function() {
-      Dom.storage.muted = music.muted = !music.muted;
-      Dom.toggleClassName('mute', 'on', music.muted);
-    });
   }
 
 }
